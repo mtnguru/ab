5ecceb50913a74e6d090e1e136d5a6f80061619b
James Sorensen <james@ecosleuth.com>
2017-02-13 06:38:00 -0700

    Fix Attach Angle

--- js/atomizer_atom.js
+++ js/atomizer_atom.js
@@ -84,14 +84,14 @@ Drupal.atomizer.atomC = function (_viewer) {
   function addNuclet(id) {
 /*  conf = {
       state: 'backbone-final',
-      nucletAngle: 3,
+      attachAngle: 3,
       protons: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],
       electrons: [0,1,2,3,4,5]
     }; */
 
     conf = {
       state: 'lithium',
-      nucletAngle: 3,
+      attachAngle: 3,
       protons: [10, 1, 4, 5, 3, 11, 9],
       electrons: [0,1,2]
     };
